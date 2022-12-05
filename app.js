8bfbd6ec81e8d859c589d1ddebf6d6f629aafb41
Jonathan <dumlao.jonathan.g.5165@gmail.com>
2022-12-05 13:25:04 +0800

    patch 1.0.1:  refractored with express

--- app.js
+++ app.js
@@ -2,8 +2,11 @@
 const http = require("http");
 const fs = require("fs");
 const url = require("url");
+const express = require("express");
+const app = express();
+const replaceTemplate = require("./modules/replaceTemplate");
 
-const PORT = 3000;
+app.use(express.json());
 
 const home = fs.readFileSync(`${__dirname}/templates/home.html`, "utf-8");
 const card = fs.readFileSync(`${__dirname}/templates/card.html`, "utf-8");
@@ -13,53 +16,30 @@ const dataObj = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8")
 );
 
-const replaceTemplate = function (temp, data) {
-  let output = temp.replace(/{%COUNTRYNAME%}/g, data.name.common);
-  output = output.replace(/{%FLAG%}/g, data.flags.svg);
-  output = output.replace(/{%CODEOFARM%}/g, data.coatOfArms.svg);
-  output = output.replace(/{%OFFICIAL%}/g, data.name.official);
-  output = output.replace(/{%CAPITAL%}/g, data.capital);
-  output = output.replace(/{%REGION%}/g, data.region);
-  output = output.replace(/{%SUBREGION%}/g, data.subregion);
-  output = output.replace(/{%MAP%}/g, data.maps.googleMaps);
-  output = output.replace(
-    /{%POPULATION%}/g,
-    new Intl.NumberFormat().format(data.population)
-  );
+// const readAPI = async function () {
+//   const res = await fetch("https://restcountries.com/v3.1/all");
+//   const data = await res.json();
 
-  return output;
+//   return data;
+// };
+const getCountry = (req, res) => {
+  const dataOut = dataObj.map((el) => replaceTemplate(card, el)).join("");
+  const out = home.replace(/{%CARD%}/, dataOut);
+  res.status(200).send(out);
 };
 
-// Create Server
+const getCountryInfo = (req, res) => {
+  const name = req.params.name;
+  const dataOut = dataObj.find((el) => el.name.common === name);
+  const infoData = replaceTemplate(info, dataOut);
+  res.status(200).send(infoData);
+};
 
-const server = http.createServer((req, res) => {
-  const { pathname, query } = url.parse(req.url, true);
-
-  // Home
-  if (pathname === "/" || pathname === "/home") {
-    res.writeHead(200, {
-      "Content-type": "text/html",
-    });
-    const dataOut = dataObj.map((el) => replaceTemplate(card, el)).join("");
-    const out = home.replace(/{%CARD%}/, dataOut);
-    res.end(out);
-  }
-
-  if (pathname === "/info") {
-    res.writeHead(200, {
-      "Content-type": "text/html",
-    });
-    console.log(query.country);
-    const dataOut = dataObj.find((el) => el.name.common === query.country);
-
-    const infoData = replaceTemplate(info, dataOut);
-    res.end(infoData);
-    // res.end("WIP");
-  }
-});
+app.route("/").get(getCountry);
+app.route("/info/:name").get(getCountryInfo);
 
 // Listen
-
-server.listen(PORT, () => {
+const PORT = 3000;
+app.listen(PORT, () => {
   console.log(`SERVER RUNNING ON PORT ${PORT}`);
 });
